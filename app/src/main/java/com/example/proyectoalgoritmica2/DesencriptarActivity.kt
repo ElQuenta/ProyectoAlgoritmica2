07c9fa49cd0990bc9dab788679b04fd1b8c5cd6d
Miguel <angeloquenta@gmail.com>
2023-11-05 15:47:46 -0400

    pantallas hechas

--- app/src/main/java/com/example/proyectoalgoritmica2/DesencriptarActivity.kt
+++ app/src/main/java/com/example/proyectoalgoritmica2/DesencriptarActivity.kt
@@ -1,11 +1,32 @@
 package com.example.proyectoalgoritmica2
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.proyectoalgoritmica2.databinding.ActivityDesencriptarBinding
 
 class DesencriptarActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDesencriptarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_desencriptar)
+        binding = ActivityDesencriptarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnAction.setOnClickListener {
+            binding.Reultado.visibility = View.VISIBLE
+        }
+        binding.fabCopyText.setOnClickListener {
+            val textoACopiar = binding.tvResultado.text
+            val clipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("Texto Copiado", textoACopiar)
+            clipboardManager.setPrimaryClip(clipData)
+        }
     }
 }
--- app/src/main/java/com/example/proyectoalgoritmica2/EncriptarActivity.kt
+++ app/src/main/java/com/example/proyectoalgoritmica2/EncriptarActivity.kt
@@ -1,11 +1,31 @@
 package com.example.proyectoalgoritmica2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectoalgoritmica2.databinding.ActivityEncriptarBinding
 
 class EncriptarActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEncriptarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_encriptar)
+        binding = ActivityEncriptarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnAction.setOnClickListener {
+            binding.Reultado.visibility = View.VISIBLE
+        }
+        binding.fabCopyText.setOnClickListener {
+            val textoACopiar = binding.tvResultado.text
+            val clipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("Texto Copiado", textoACopiar)
+            clipboardManager.setPrimaryClip(clipData)
+        }
     }
 }
